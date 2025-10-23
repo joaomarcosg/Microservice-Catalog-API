7b00095850dabd53179e3aa03f7621cbb4fe2162
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-10-23 15:21:47 -0300

    CHORE: initial commit

---
+++ Entities/Product.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog_Microservice.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set;}
+    }
+}
